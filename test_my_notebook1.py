4106206dbadd5934be0a2d710f5b6a55c4a40e3d
Alicia Fritz <amfritz2@asu.edu>
2024-02-20 19:48:38 -0700

    Updated test names for 3 different tests and removed spaces

--- test_my_notebook1.py
+++ test_my_notebook1.py
@@ -1,35 +1,35 @@
 from testbook import testbook
 import pandas as pd
 
-# define test function using Testbook for first test of regular data
+# define test function using Testbook for first test using regular data
 @testbook('Feature1-Network-Analysis.ipynb', execute=True)
-def test_create_graph(tb):
-    # Retrieve the create_graph function from the notebook
+def test_create_graph_regular_data(tb):
+    # retrieve the create_graph function from the notebook
     create_graph = tb.ref("create_graph")
 
-    # Call create_graph with the test data CSV file path
+    # call create_graph with the test data CSV file path
     test_data_path = "/Users/aliciafritz/Desktop/TestData.csv"
     my_graph = create_graph(test_data_path)
 
-    # Perform assertions to test the functionality
+    # perform assertions to test the functionality
     assert len(my_graph.nodes()) == 3
 
-# define test function using Testbook for second test of missing data
+# define test function using Testbook for second test using missing data
 @testbook('Feature1-Network-Analysis.ipynb', execute=True)
-def test_create_graph(tb):
-    # Retrieve the create_graph function from the notebook
+def test_create_graph_missing_data(tb):
+    # retrieve the create_graph function from the notebook
     create_graph = tb.ref("create_graph")
 
-    # Call create_graph with the test data CSV file path
+    # call create_graph with the test data CSV file path
     test_data_path = "/Users/aliciafritz/Desktop/MissingTestData.csv"
     my_graph = create_graph(test_data_path)
 
-    # Perform assertions to test the functionality
+    # perform assertions to test the functionality
     assert len(my_graph.nodes()) == 3
 
-# define test function using Testbook for third test of duplicated data
+# define test function using Testbook for third test using duplicated data
 @testbook('Feature1-Network-Analysis.ipynb', execute=True)
-def test_create_graph(tb):
+def test_create_graph_duplicate_data(tb):
     # Retrieve the create_graph function from the notebook
     create_graph = tb.ref("create_graph")
 
